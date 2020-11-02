4d4c7cc484c4e9f202b061339baba5e2a590f8b2
Vinicius Pelissari Jantorno <vinicius.pelissari@gmail.com>
2020-11-02 13:32:42 -0300

    Classes temporarias apenas para testar consultas.

---
+++ grandpet-api/src/main/java/br/com/grandpet/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,33 @@
+package br.com.grandpet.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+import br.com.grandpet.GrandpetApiApplication;
+import br.com.grandpet.domain.model.Cozinha;
+
+public class ConsultaCozinhaMain {
+
+	public static void main(String[] args) {
+		
+		// Inicia uma aplicacao nao web, diferente da classe GrandpetApiApplication, que espera requisicoes
+		// Inicia nossa app springboot como nao sendo uma app web.
+		// Lembrar de rodar como Run as -> Java Aplication
+		ApplicationContext applicationContext = new SpringApplicationBuilder(GrandpetApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		// Com o trecho acima eh possivel pegar um Bean da minha aplicacao
+		// Pegando um Bean do tipo CadastroCozinha
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		
+		for (Cozinha cozinha : cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
+	}
+}
