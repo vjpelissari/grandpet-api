5d940f95757b373ddfbaf7b93c01f07eba4fbdc0
Vinicius Pelissari Jantorno <vinicius.pelissari@gmail.com>
2020-11-04 21:55:16 -0300

    Alteracao nome dos metodos.

---
+++ grandpet-api/src/main/java/br/com/grandpet/domain/repository/CozinhaRepository.java
@@ -0,0 +1,16 @@
+package br.com.grandpet.domain.repository;
+
+import java.util.List;
+
+import br.com.grandpet.domain.model.Cozinha;
+
+public interface CozinhaRepository {
+	
+	List<Cozinha> todas();
+
+	Cozinha porId(Long id);
+
+	Cozinha adicionar(Cozinha cozinha);
+
+	void remover(Cozinha cozinha);
+}
---
+++ grandpet-api/src/main/java/br/com/grandpet/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,44 @@
+package br.com.grandpet.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.grandpet.domain.model.Cozinha;
+import br.com.grandpet.domain.repository.CozinhaRepository;
+
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Cozinha> todas() {
+		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
+	}
+	
+	@Override
+	public Cozinha porId(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+	
+	@Transactional
+	@Override
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+	}
+	
+	@Transactional
+	@Override
+	public void remover(Cozinha cozinha) {
+		cozinha = porId(cozinha.getId());
+		manager.remove(cozinha);
+	}
+}
