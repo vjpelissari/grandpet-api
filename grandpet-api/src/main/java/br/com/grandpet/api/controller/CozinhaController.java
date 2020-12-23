a38560836c9b62c8866a5700905794a1395239ab
Vinicius Pelissari Jantorno <vinicius.pelissari@gmail.com>
2020-12-22 21:25:31 -0300

    Retirando suporte a XML.

--- grandpet-api/src/main/java/br/com/grandpet/api/controller/CozinhaController.java
+++ grandpet-api/src/main/java/br/com/grandpet/api/controller/CozinhaController.java
@@ -15,10 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-import br.com.grandpet.api.model.CozinhasXmlWrapper;
 import br.com.grandpet.domain.model.Cozinha;
 import br.com.grandpet.domain.repository.CozinhaRepository;
 import br.com.grandpet.domain.service.CadastroCozinhaService;
@@ -38,13 +36,6 @@ public class CozinhaController {
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
-	
-	
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());
-	}
-	
 	
 	// {cozinhaId} --> Path Variable
 	@GetMapping("{cozinhaId}") 
@@ -75,7 +66,7 @@ public class CozinhaController {
 		if (cozinhaAtual != null) {
 			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
 			
-			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			cozinhaAtual = cadastroCozinha.salvar(cozinhaAtual);
 			return ResponseEntity.ok(cozinhaAtual);
 		}
 		return ResponseEntity.notFound().build();
